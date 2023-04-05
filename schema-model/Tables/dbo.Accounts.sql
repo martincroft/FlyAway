570465cefbfdcf9f87bf4014c7628fc23e6973c0
Martin Croft <martin.croft@sqlserversolutions.co.uk>
2023-04-05 18:20:27 +0100

    Addded monthlt figures

--- schema-model/Tables/dbo.Accounts.sql
+++ schema-model/Tables/dbo.Accounts.sql
@@ -3,7 +3,8 @@ CREATE TABLE [dbo].[Accounts]
 [Id] [int] NOT NULL,
 [AccountName] [varchar] (20) NULL,
 [Status] [varchar] (10) NULL,
-[Balance] [decimal] (12, 2) NULL
+[Balance] [decimal] (12, 2) NULL,
+[PreviousMonthBalance] [decimal] (12, 2) NULL
 )
 GO
 ALTER TABLE [dbo].[Accounts] ADD CONSTRAINT [PK__Accounts__3214EC071537E4F5] PRIMARY KEY CLUSTERED ([Id])
