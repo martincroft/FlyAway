bfbdc3ac9a9e984774cb1faaa88de995fe086086
Martin Croft <martin.croft@sqlserversolutions.co.uk>
2023-04-11 16:10:44 +0100

    Changes demo

--- schema-model/Tables/dbo.Accounts.sql
+++ schema-model/Tables/dbo.Accounts.sql
@@ -4,7 +4,8 @@ CREATE TABLE [dbo].[Accounts]
 [AccountName] [varchar] (20) NULL,
 [Status] [varchar] (10) NULL,
 [Balance] [decimal] (12, 2) NULL,
-[PreviousMonthBalance] [decimal] (12, 2) NULL
+[PreviousMonthBalance] [decimal] (12, 2) NULL,
+[TEST] [int] NULL
 )
 GO
 ALTER TABLE [dbo].[Accounts] ADD CONSTRAINT [PK__Accounts__3214EC071537E4F5] PRIMARY KEY CLUSTERED ([Id])
