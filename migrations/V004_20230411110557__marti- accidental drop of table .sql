34c32644bbbefc973e2818e5100cf0078a8da332
Martin Croft <martin.croft@sqlserversolutions.co.uk>
2023-04-11 12:07:15 +0100

    Script to fix accidental drop table

---
+++ migrations/V004_20230411110557__marti- accidental drop of table .sql
@@ -0,0 +1,20 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Creating [dbo].[Accounts]'
+GO
+CREATE TABLE [dbo].[Accounts]
+(
+[Id] [int] NOT NULL,
+[AccountName] [varchar] (20) NULL,
+[Status] [varchar] (10) NULL,
+[Balance] [decimal] (12, 2) NULL,
+[PreviousMonthBalance] [decimal] (12, 2) NULL
+)
+GO
+PRINT N'Creating primary key [PK__Accounts__3214EC071537E4F5] on [dbo].[Accounts]'
+GO
+ALTER TABLE [dbo].[Accounts] ADD CONSTRAINT [PK__Accounts__3214EC071537E4F5] PRIMARY KEY CLUSTERED ([Id])
+GO
+
